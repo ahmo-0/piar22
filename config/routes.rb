3c15239d03971f23c2dfe67404ddbc95cc2fc768
ahmo <ahmedmuminovic@yahoo.co>
2020-05-06 00:24:28 +0200

    sve

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  devise_for :users
+  root to: 'posts#index'
+
+  mount Commontator::Engine => '/commontator'
+
+  resources :posts
+
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
